a45f04e4c4e094a77a68528c993c2f46c734d39f
2001mapa <arrovelez123@gmail.com>
2023-08-15 17:06:24 -0500

    cambios
    falta las toneladas

--- TallerLogica/src/taller/Solucion.java
+++ TallerLogica/src/taller/Solucion.java
@@ -8,10 +8,53 @@ package taller;
  *
  * @author 503
  */
+import java.util.Scanner;
 public class Solucion {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");
+      int Operacion1 = 3+((8*5)-(6/3));
+        System.out.println("El resultado es " + Operacion1);
+        
+
+      double Operacion2 = ((2.5*2*3)-(4/2)+8);
+         System.out.println("El resultado es " + Operacion2);
+        
+      double  Operacion3= (2*(6-2.5))/3;
+        System.out.println("El resultado es " + Operacion3);
+        
+     boolean Operacion4 = 25>18;
+      System.out.println("El resultado es " + Operacion4);
+      
+      boolean Operacion5 = ((3+2.5)*4)/3 != (4*5.2)-3.8;
+       System.out.println("El resultado es " + Operacion5);
+       
+      boolean Operacion6 = (8.5/3.2+6.5-(2.3*5)/6 >= 15.8) && (21.4<81/4+2);
+       System.out.println("El resultado es " + Operacion6);
+        System.out.println("--------------------------");
+      Rectangulo();
+       
+    }
+    
+    
+    public static void Rectangulo (){
+     
+        double base, altura;
+        System.out.println("Ingrese la base: ");
+        base = sc.nextDouble();
+        System.out.println("ingrese la altura: ");
+        altura = sc.nextDouble();
+      
+        double area = base*altura;
+        System.out.println("El area del rectangulo es " + area + " cm2");
+        
+       
+        double perimetro = (base*2) + (altura*2);
+          System.out.println("El perimetro del rectangulo es " + perimetro + " cm");
+    }
+        
+    public static void Carbon(){
+    
     }
     
 }
