83b8760e93cd9d712152781742f7b6100d0b58a6
garroyve <arrovelez123@gmail.com>
2023-08-18 00:19:02 -0500

    cambios
    subir

--- TallerLogica/src/taller/Solucion.java
+++ TallerLogica/src/taller/Solucion.java
@@ -33,8 +33,13 @@ public class Solucion {
        System.out.println("El resultado es " + Operacion6);
         System.out.println("--------------------------");
       Rectangulo();
-       
-    }
+      System.out.println("--------------------------");
+      Carros();
+      System.out.println("--------------------------");
+      AppB();
+      System.out.println("--------------------------");
+      Numeros();
+    }   
     
     
     public static void Rectangulo (){
@@ -56,5 +61,74 @@ public class Solucion {
     public static void Carbon(){
     
     }
+    public static void Carros () {
+        double comision;
+        System.out.println("Ingrese el nombre del auto vendido: ");
+       String  autoVend = sc.next();
+        System.out.println("ingrese el precio del auto : ");
+       int   precio = sc.nextInt();
+       
+       if(precio <=100000){
+           comision = (precio *0.015);
+           System.out.println("Su comision es de : " + comision);
+       }else if(precio >100000 && precio <=250000){
+           comision = (precio * 0.018);
+           System.out.println("Su comision es de : " + comision);
+       }else if(precio >250000){
+           comision = (precio * 0.023);
+           System.out.println("Su comision es de : " + comision);
+       }
+    
+    
+        
+    }
+    
+    public static void AppB() {
+         System.out.println("BIENVENIDO " + "\n" +
+                 "----------- " + "\n" +
+                 "ingrese 1 si quiere consultar su saldo " + "\n" +
+                 "ingrese 2 si desea realizar transaciones " + "\n" +
+                 "ingrese 3 si desea cambiar la clave " + "\n" +
+                 "ingrese 4 para bloquear producto " + "\n" +
+                 "ingrese 5 para donaciones  " + "\n"  );
+                 
+            int   opcion = sc.nextInt();   
+            
+            switch (opcion) {
+            case 1:
+                System.out.println("su saldo es : $$$$$$$" );
+                break;
+            case 2:
+                System.out.println("trasacion exitosa" );
+                break;  
+            case 3 :
+                System.out.println("cambio de cable exitosa" );
+                break;   
+                case 4:
+                System.out.println("producto bloqueado" );
+                break;
+            case 5:
+                System.out.println("Donacion realizada" );
+                break;    
+            default:
+                System.out.println("error intenta de nuevo ");
+        }
+            
+    
+                 
+                
+                      
+    }
+    
+    public static void Numeros() {
+        int contador = 0;
+        
+        for(int i = 0; i <100; i++){
+            contador = contador +2;
+            System.out.println(contador );
+        }
+        
+    }
+    
     
 }
